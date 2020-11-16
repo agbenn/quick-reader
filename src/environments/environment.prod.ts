21b72f8a65c0af57baa9ba97811c4f6b65f87c8c
agbenn <10998275+agbenn@users.noreply.github.com>
2020-11-16 01:18:19 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAAjKWq7Ln0_r-gF-ZIf3NKW6FtjFPaV2Q",
-    authDomain: "my-project-1484875067263.firebaseapp.com",
-    databaseURL: "https://my-project-1484875067263.firebaseio.com",
-    projectId: "my-project-1484875067263",
-    storageBucket: "my-project-1484875067263.appspot.com",
-    messagingSenderId: "310507400759",
-    appId: "1:310507400759:web:30ed129ff75a868bc059b2",
-    measurementId: "G-MVDH94GS2Z"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
